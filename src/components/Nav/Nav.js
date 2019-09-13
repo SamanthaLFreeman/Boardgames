6f4722bc0b4cbf0e4f09ec405f432570bffc7a36
Alan Turing <freemasl.sf@gmail.com>
2019-09-12 21:10:04 -0600

    Set up the Nav

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Nav = () => {
+  return (
+    <nav>
+      <h1>Deck Building</h1>
+      <button>View Favorites</button>
+      <button>Owned Collection</button>
+      <button>Search</button>
+    </nav>
+  )
+}
+
+export default Nav;

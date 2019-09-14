a3816d9f19766bd3bebcd2a7d6c3671016a78600
Alan Turing <freemasl.sf@gmail.com>
2019-09-14 09:42:08 -0600

    Add happy and sad tests for apiCalls searchGames method

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -1,4 +1,4 @@
-import { getPopularGames, getAllCategories } from './apiCalls';
+import { getPopularGames, getAllCategories, searchGames } from './apiCalls';
 
 describe('getPopularGame', () => {
   let mockGames;
@@ -54,8 +54,7 @@ describe('getPopularGame', () => {
 
     expect(getPopularGames()).rejects.toEqual(Error('fetch failed'))
   });
-
-})
+});
 
 describe('getAllCategories', () => {
   let mockCategories;
@@ -80,7 +79,7 @@ describe('getAllCategories', () => {
     expect(window.fetch).toHaveBeenCalledWith('https://www.boardgameatlas.com/api/game/categories?client_id=igbjxf77TF')
   });
 
-  it('should return the array of popular games', () => {
+  it('should return the array of all categories with ids', () => {
     expect(getAllCategories()).resolves.toEqual(mockCategories);
   });
 
@@ -100,5 +99,63 @@ describe('getAllCategories', () => {
     })
 
     expect(getAllCategories()).rejects.toEqual(Error('fetch failed'))
+  });
+});
+
+describe('searchGames', () => {
+  let mockGames;
+  let mockSearch;
+
+  beforeEach(() => {
+    mockGames = [{
+      id: 42,
+      name: "Catan",
+      year_published: 1999,
+      min_players: 2,
+      max_players: 6,
+      min_playtime: 30,
+      max_playtime: 60,
+      description_preview: "Gotta get all the sheep",
+      image_url: "image",
+      primary_publisher: "me",
+      categories: [{ id: "1234" }],
+      rules_url: "rules link"
+    }]
+    mockSearch = "Catan"
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockGames)
+      })
+    })
+  });
+
+  it('should call fetch with the correct url', () => {
+    searchGames(mockSearch);
+
+    expect(window.fetch).toHaveBeenCalledWith('https://www.boardgameatlas.com/api/search?name=Catan&client_id=igbjxf77TF')
+  });
+
+  it('should return the array of searched games', () => {
+    expect(searchGames(mockSearch)).resolves.toEqual(mockGames);
+  });
+
+  it('should return an error if the promise resolves but the property ok isn\'t true', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      })
+    })
+
+    expect(searchGames()).rejects.toEqual(Error('Error fetching games'))
+  });
+
+  it('should return an error if the promise rejects', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('fetch failed'))
+    })
+
+    expect(searchGames(mockSearch)).rejects.toEqual(Error('fetch failed'))
   });
 });

2b2ec1bf71cf27d6662268d2c9f528377ce6d2b4
Alan Turing <freemasl.sf@gmail.com>
2019-09-14 20:25:19 -0600

    Add tests to CardDetails to simulate change in input and button click for updated the game

--- src/components/CardDetails/CardDetails.test.js
+++ src/components/CardDetails/CardDetails.test.js
@@ -4,7 +4,10 @@ import { CardDetails } from './CardDetails';
 
 describe('CardDetails', () => {
   let wrapper;
-  const mockCard = {}
+  const mockCard = {
+    name: 'Takenoko',
+    id: 41
+  }
   const categoriesIdsMock = [
     {
       id: 1234
@@ -27,6 +30,7 @@ describe('CardDetails', () => {
   beforeEach(() => {
     wrapper = shallow(<CardDetails 
     card={mockCard}
+    id={41}
     categoriesIds={categoriesIdsMock}
     categoriesKey={categoriesKeyMock}
     ownedGames={ownedGamesMock}
@@ -35,5 +39,23 @@ describe('CardDetails', () => {
 
   it('should match the snapshot with the data passed through', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state when handleChange is called', () => {
+    const mockEvent = { target: { name: 'house_rules', value: 'Start with 7 cards' } };
+    const expected = 'Start with 7 cards';
+
+    wrapper.instance().handleChange(mockEvent);
+
+    expect(wrapper.state('house_rules')).toEqual(expected);
+  });
+
+  it('should run handleSubmit when the button is clicked', () => {
+    wrapper.instance().handleSubmit = jest.fn();
+    const mockEvent = { preventDefault: jest.fn() };
+    wrapper.instance().forceUpdate();
+    wrapper.find('button').simulate('click', mockEvent);
+
+    expect(wrapper.instance().handleSubmit).toHaveBeenCalled();
   });
 })
